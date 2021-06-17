f4381222c61c70bb363f70f88a75d6704f45bcd8
vjohnsonm <72634958+vjohnsonm@users.noreply.github.com>
2021-06-17 08:54:36 -0700

    Made minor edits

--- src/main/java/helloworld/HelloWorld.java
+++ src/main/java/helloworld/HelloWorld.java
@@ -1,9 +1,16 @@
 package helloworld;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class HelloWorld {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World - Merry christmas to all and Happy new year too");
-	}
+  public static void main(final String[] args) {
+    final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
+    final LocalDateTime now = LocalDateTime.now();
+
+    System.out.println("\r\nHello, World! The current time is " + dtf.format(now));
+    System.out.println("\r\nHave a nice time");
+  }
 
 }
